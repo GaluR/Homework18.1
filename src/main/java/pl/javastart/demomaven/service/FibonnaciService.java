46a3001c137fe5b54c8e3bac177e6a25da13c3ce
Bartosz Galinski <galinski.bartosz@gmail.com>
2019-09-04 14:39:40 +0200

    Change arrays.

--- src/main/java/pl/javastart/demomaven/service/FibonnaciService.java
+++ src/main/java/pl/javastart/demomaven/service/FibonnaciService.java
@@ -1,22 +1,24 @@
 package pl.javastart.demomaven.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonnaciService {
 
     public int sumOfFibonacci(int maximumValue) {
-        int[] fibonacciNumbers = new int[100];
-        fibonacciNumbers[0] = 1;
-        fibonacciNumbers[1] = 1;
+        List<Integer> fibonacciNumbers = new ArrayList<>();
+        fibonacciNumbers.add(1);
+        fibonacciNumbers.add(1);
         int sum = 0;
-        for (int i = 1; i < fibonacciNumbers.length-1; i++) {
-            fibonacciNumbers[i + 1] = fibonacciNumbers[i] + fibonacciNumbers[i - 1];
-            if (fibonacciNumbers[i + 1] % 2 == 0) {
-                sum += fibonacciNumbers[i + 1];
-                if (fibonacciNumbers[i+1] > maximumValue) {
-                    sum = sum - fibonacciNumbers[i + 1];
+        for (int i = 1; i < fibonacciNumbers.size(); i++) {
+            fibonacciNumbers.add(fibonacciNumbers.get(i) + fibonacciNumbers.get(i - 1));
+            if (fibonacciNumbers.get(i + 1) % 2 == 0) {
+                sum += fibonacciNumbers.get(i + 1);
+                if (fibonacciNumbers.get(i + 1) > maximumValue) {
+                    sum = sum - fibonacciNumbers.get(i + 1);
                     break;
                 }
             }
-
         }
         return sum;
     }
